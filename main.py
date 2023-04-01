3bde23898f49582d088c1679aa14474a9ccda355
kai1130 <51947774+kai1130@users.noreply.github.com>
2023-04-01 16:07:37 -0700

    improvements

--- main.py
+++ main.py
@@ -12,43 +12,50 @@ def confidence(cos_sim):
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
+st.header('Zero-Shot Text Classification')
+
 col1, col2 = st.columns(2)
 
 with col1:
-    message_input = st.text_input(
+    st.subheader('Type a Message')
+    message_input = st.text_area(
         label='Input message here',
         value='Hi, how much does your product cost?',
         key="placeholder"
     )
     if message_input:
         message = message_input
-        st.write(f"Message: {message}")
+        st.caption(f"Current Message")
+        st.write(f'{message}')
 
 with col2:
-    category_input = st.text_input(
+    st.subheader('Define Categories')
+    category_input = st.text_area(
         label='Desired categories (separated by commas)',
-        value='''
-        Price or Product Inquiry, Order Placement, Appointment Scheduling
-        ''',
+        value="Price or Product Inquiry\nOrder Placement\nAppointment Scheduling",
         key='categories'
     )
 
     if category_input:
         categories = [c.strip() for c in category_input.split(',')]
-        st.write('Categories')
+        st.caption('Current Categories')
         st.markdown(categories)
 
-message_embedding = model.encode([message])
-category_embeddings = model.encode(categories)
+classify = st.button(label='Classify Message')
 
-categories_map = {k: v for k, v in enumerate(categories)}
+if classify:
 
-category_similarity = util.cos_sim(message_embedding, category_embeddings)
+    message_embedding = model.encode([message])
+    category_embeddings = model.encode(categories)
 
-categories_argmax = int(category_similarity.argmax(axis=1)[0])
-categories_str = categories_map.get(categories_argmax, None)
+    categories_map = {k: v for k, v in enumerate(categories)}
 
-categories_max = float(category_similarity.max(axis=1)[0][0])
-categories_confidence = confidence(categories_max)
+    category_similarity = util.cos_sim(message_embedding, category_embeddings)
 
-st.write('AutoCategory: {categories_str} | Confidence: {categories_confidence}%')
+    categories_argmax = int(category_similarity.argmax(axis=1)[0])
+    categories_str = categories_map.get(categories_argmax, None)
+
+    categories_max = float(category_similarity.max(axis=1)[0][0])
+    categories_confidence = confidence(categories_max)
+
+    st.write(f'AutoCategory: {categories_str} | Confidence: {categories_confidence}%')
